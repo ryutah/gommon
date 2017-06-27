d0347609c49357d1a62510b186e2a84622272a32
ryuta <ryuta.hara@topgate.co.jp>
2017-06-27 13:07:21 +0900

    change shuffle function to return copy list

--- slice/shuffle.go
+++ slice/shuffle.go
@@ -1,23 +1,21 @@
 package slice
 
 import (
-	"fmt"
 	"math/rand"
 	"reflect"
 	"time"
 )
 
-func Shuffle(slice interface{}) {
-	if sliceType := reflect.TypeOf(slice); sliceType.Kind() != reflect.Slice {
-		panic(fmt.Sprintf("%v is not slice", slice))
-	}
-
-	sliceVal := reflect.ValueOf(slice)
-	for i := 0; i < sliceVal.Len(); i++ {
+func Shuffle(slice interface{}) interface{} {
+	sliceType, sliceVal := reflect.TypeOf(slice), reflect.ValueOf(slice)
+	retSlice := reflect.MakeSlice(sliceType, sliceVal.Len(), sliceVal.Cap())
+	reflect.Copy(retSlice, sliceVal)
+	for i := 0; i < retSlice.Len(); i++ {
 		rand.Seed(time.Now().UnixNano())
 		j := rand.Intn(i + 1)
-		buf := sliceVal.Index(i).Interface()
-		sliceVal.Index(i).Set(sliceVal.Index(j))
-		sliceVal.Index(j).Set(reflect.ValueOf(buf))
+		buf := retSlice.Index(i).Interface()
+		retSlice.Index(i).Set(retSlice.Index(j))
+		retSlice.Index(j).Set(reflect.ValueOf(buf))
 	}
+	return retSlice.Interface()
 }
--- slice/shuffle_test.go
+++ slice/shuffle_test.go
@@ -12,21 +12,25 @@ func TestShuffle(t *testing.T) {
 	want := make([]int, len(arr))
 	copy(want, arr)
 
-	slice.Shuffle(arr)
+	got := slice.Shuffle(arr).([]int)
 
-	for _, av := range arr {
+	if !reflect.DeepEqual(arr, want) {
+		t.Errorf("given array must not be changed. source array %v, want %v", arr, want)
+	}
+
+	for _, g := range got {
 		for i := 0; i < len(want); i++ {
-			if av == want[i] {
+			if g == want[i] {
 				want = append(want[:i], want[i+1:]...)
 			}
 		}
 	}
 
-	if reflect.DeepEqual(arr, want) {
+	if reflect.DeepEqual(got, want) {
 		t.Fatal("array did not be shuffled")
 	}
 
 	if len(want) != 0 {
-		t.Errorf("remains %v, array : %v", want, arr)
+		t.Errorf("remains %v, array : %v", want, got)
 	}
 }
